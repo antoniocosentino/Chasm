17423f7e3bd886c9c0213204406e5c02318609d7
antoniocosentino <hello@antoniocosentino.it>
2017-08-30 00:06:38 +0200

    added new indicators and cases

--- chasm.cs
+++ chasm.cs
@@ -3,7 +3,7 @@
 ///   Description:      cAlgo trading bot
 ///   Author:           Antonio Cosentino
 ///   Version:          1.0
-///   Updated:          13/08/2017
+///   Updated:          29/08/2017
 ///-----------------------------------------------------------------
 
 using System;
@@ -22,35 +22,32 @@ namespace cAlgo.indicators
     {
         string version_number = "1.0";
         // Declarations
-        private bool is_position_open;
-        private double init_close_value;
         private int currenthour;
         private int currentminute;
         private string stringdate;
-        private int previous_dayotw;
-        private bool day_enable = false;
-        private double yesterday_high;
-        private double yesterday_low;
+
+        private bool is_position_open;
+        private string scenario;
+
+        private double today_open;
         private double today_high;
         private double today_low;
         private double bid_price;
-        private double today_open_price;
-        private bool daily_check_done;
-        private string scenario;
+        private double ask_price;
+        private double yesterday_close;
+        private double yesterday_high;
+        private double yesterday_low;
         private int kindex;
         ///
-
-        [Parameter("Start time (hh)", DefaultValue = 9, MinValue = 1, MaxValue = 24)]
-        public int start_time_hh { get; set; }
-
-        [Parameter("Start time (mm)", DefaultValue = 0, MinValue = 0, MaxValue = 59)]
-        public int start_time_mm { get; set; }
 
         [Parameter("LONG Trailing Stop (%)", DefaultValue = 8, MinValue = 1, MaxValue = 50)]
         public int long_stop { get; set; }
 
         [Parameter("SHORT Trailing Stop (%)", DefaultValue = 4, MinValue = 1, MaxValue = 50)]
         public int short_stop { get; set; }
+
+        [Parameter("Diff Ticks", DefaultValue = 1, MinValue = 1E-07, MaxValue = 1000)]
+        public double diffticks { get; set; }
 
         [Parameter("GAP Up Long?", DefaultValue = true)]
         public bool gap_up_mode { get; set; }
@@ -60,74 +57,94 @@ namespace cAlgo.indicators
 
         protected override void OnStart()
         {
-            init_close_value = MarketSeries.Close.LastValue;
-            previous_dayotw = -1;
             is_position_open = false;
-            daily_check_done = false;
+            kindex = 0;
 
             Print("Chasm {0} Started", version_number);
-            Print("Server time is {0}", Server.Time.AddHours(2));
+            Print("Server time is {0}", Server.Time.AddHours(0));
         }
 
         protected override void OnBar()
         {
-            if (previous_dayotw == -1 || (int)Server.Time.DayOfWeek != previous_dayotw)
+            kindex = MarketSeries.Close.Count - 1;
+            today_open = MarketSeries.Open[kindex];
+            yesterday_close = MarketSeries.Close[kindex - 1];
+            yesterday_high = MarketSeries.High[kindex - 1];
+            yesterday_low = MarketSeries.Low[kindex - 1];
+
+
+            Print("Open price is: {0}", today_open);
+            Print("Yesterday Close is: {0}", yesterday_close);
+            Print("Yesterday High is: {0}", yesterday_high);
+            Print("Yesterday Low is: {0}", yesterday_low);
+
+
+            if (today_open > yesterday_close)
             {
-                // Day has changed
-                previous_dayotw = (int)Server.Time.DayOfWeek;
-                if (previous_dayotw != 6 && previous_dayotw != 0)
-                {
-                    day_enable = true;
-                    daily_check_done = false;
-                }
+                scenario = "GAPUP";
             }
+            else if (today_open < yesterday_close)
+            {
+                scenario = "GAPDOWN";
+            }
+            else
+            {
+                scenario = null;
+            }
+
+            if (scenario != null)
+            {
+                Print("Scenario is: {0}", scenario);
+                Timer.Start(3600);
+            }
+
+
+
+
+
+        }
+
+        protected override void OnTimer()
+        {
+            Timer.Stop();
+            today_high = MarketSeries.High[kindex];
+            today_low = MarketSeries.Low[kindex];
+
+            Print("Today High is: {0}", today_high);
+            Print("Today Low is: {0}", today_low);
+
+            if (scenario == "GAPUP" && gap_up_mode)
+            {
+                Print("Buy and close at {0}", today_high + diffticks);
+            }
+            else if (scenario == "GAPUP" && !gap_up_mode)
+            {
+                Print("Sell and close at {0}", today_low - diffticks);
+            }
+            else if (scenario == "GAPDOWN" && gap_down_mode)
+            {
+                Print("Buy and close at {0}", yesterday_low + diffticks);
+            }
+            else if (scenario == "GAPDOWN" && !gap_down_mode)
+            {
+                Print("Sell and close at {0}", today_low - diffticks);
+            }
+
+
+            Print("- - -");
         }
 
         protected override void OnTick()
         {
             // Time Vars
-            stringdate = Server.Time.AddHours(2).ToString("HH:mm");
+            stringdate = Server.Time.AddHours(0).ToString("HH:mm");
             currenthour = int.Parse(stringdate.Substring(0, 2));
             currenthour = Convert.ToInt32(stringdate.Substring(0, 2));
             currentminute = int.Parse(stringdate.Substring(3, 2));
             currentminute = Convert.ToInt32(stringdate.Substring(3, 2));
             ///
-
             bid_price = Symbol.Bid;
-
-            if (day_enable)
-            {
-                if ((currenthour >= start_time_hh && currentminute >= start_time_mm) && daily_check_done == false)
-                {
-                    kindex = MarketSeries.Close.Count - 1;
-                    today_open_price = MarketSeries.Open[kindex];
-                    yesterday_high = MarketSeries.High[kindex - 1];
-                    yesterday_low = MarketSeries.Low[kindex - 1];
-
-                    if (today_open_price > yesterday_high)
-                    {
-                        scenario = "FULL_GAP_UP";
-                    }
-                    else if (today_open_price < yesterday_low)
-                    {
-                        scenario = "FULL_GAP_DOWN";
-                    }
-                    else
-                    {
-                        scenario = null;
-                    }
-                    Print(scenario);
-                    Print("Today open price is {0}, yesterday high is {1}, yesterday low is {2}", today_open_price, yesterday_high, yesterday_low);
-                    daily_check_done = true;
-                }
-            }
-
-
-
-
-
-
-
+            ask_price = Symbol.Ask;
 
         }
 
